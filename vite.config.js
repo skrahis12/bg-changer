72ef6a4058e10bb69ba1b46972a3aa32449f1665
skrahis12 <skrahism@gmail.com>
2026-02-23 09:27:35 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss()],
   base:"/bg-changer/",
-  build: {
-    outDir: 'dist',
-  },
+ 
 })
